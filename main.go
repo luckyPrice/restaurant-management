0ab51fe865fce155fc402d5fe9b7cf136dc93e49
luckyPrice <luckyprice1103@naver.com>
2024-08-24 02:35:49 +0900

    create routes

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"golang-restaurant-management/database"
+	"golang-restaurant-management/middleware"
+	"golang-restaurant-management/routes"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var foodcollection *mongo.Collection = database.Opencollection(database.Cliet, "food")
+
+func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8080"
+	}
+
+	router := gin.New()
+	router.User(gin.Logger())
+	routes.UserRoutes(router)
+	router.Use(middleware.Authentication())
+
+	routes.FoodRoutes(router)
+	routes.MenuRoutes(router)
+	routes.TableRoutes(router)
+	routes.OrderRoutes(router)
+	routes.InvoiceRoutes(router)
+
+	router.Run(":" + port)
+}
