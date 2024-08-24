7502f830e78800f207e9b8fbfc6f768503570428
luckyPrice <luckyprice1103@naver.com>
2024-08-24 23:53:17 +0900

    create controllers

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
-var foodcollection *mongo.Collection = database.Opencollection(database.Cliet, "food")
+var foodcollection *mongo.Collection = database.Opencollection(database.Client, "food")
 
 func main() {
 	port := os.Getenv("PORT")
--- routes/tableRouter.go
+++ routes/tableRouter.go
@@ -8,7 +8,7 @@ import (
 
 func TableRoutes(incomingRoutes *gin.Engine) {
 	incomingRoutes.GET("/tables", controller.GetTables())
-	incomingRoutes.GET("/tables/:table_id", controller.GetTalbe())
+	incomingRoutes.GET("/tables/:table_id", controller.GetTable())
 	incomingRoutes.POST("/tables", controller.CreateTable())
 	incomingRoutes.PATCH("/tables/:table_id", controller.UpdateTable())
 
